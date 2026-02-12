4eb0041aa8d59483638ad0aa2b623d3c6a8b40bb
methy <methymuy@gmail.com>
2026-02-12 14:38:26 +0700

    feat:Bla-001 implement rides service filter methods

--- lib/services/rides_service.dart
+++ lib/services/rides_service.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 import '../dummy_data/dummy_data.dart';
 import '../model/ride/locations.dart';
 import '../model/ride/ride.dart';
@@ -9,9 +11,8 @@ class RidesService {
   //  filter the rides starting from given departure location
   //
   static List<Ride> _filterByDeparture(List<Ride> rides, Location departure) {
-    return rides.where((rides)=> rides.arrivalLocation == departure).toList();
+    return rides.where((ride) => ride.departureLocation == departure).toList();
   }
-
 
   //
   //  filter the rides starting for the given requested seat number
@@ -20,17 +21,23 @@ class RidesService {
     List<Ride> rides,
     int requestedSeat,
   ) {
-     return rides.where((rides)=> rides.availableSeats > requestedSeat).toList();
-    
+    return rides.where((ride) => ride.availableSeats >= requestedSeat).toList();
   }
 
   //
   //  filter the rides   with several optional criteria (flexible filter options)
   //
-  static List<Ride> filterBy({Location? departure, int? seatRequested, required int seatsRequested}) {
-    List<Ride> result = List.from(availableRides);
-    if(departure !=null){
-      return result _filterByDeparture();
+  static List<Ride> filterBy({Location? departure, int? seatRequested}) {
+    List<Ride> result = availableRides;
+
+    if (departure != null) {
+      result = _filterByDeparture(result, departure);
     }
+
+    if (seatRequested != null) {
+      result = _filterBySeatRequested(result, seatRequested);
+    }
+
+    return result;
   }
 }
